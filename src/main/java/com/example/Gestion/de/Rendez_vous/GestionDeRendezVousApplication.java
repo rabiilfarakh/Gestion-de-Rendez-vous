60543267e92276d759b2217cc1960cf88dc59aa5
rabiilfarakh <rabiilfarakh2816@gmail.com>
2024-08-03 17:22:38 +0100

    first commit

---
+++ src/main/java/com/example/Gestion/de/Rendez_vous/GestionDeRendezVousApplication.java
@@ -0,0 +1,13 @@
+package com.example.Gestion.de.Rendez_vous;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeRendezVousApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeRendezVousApplication.class, args);
+	}
+
+}
